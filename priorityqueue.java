8e1be752872d9dedd48cdefa8dc04c28f3ae6a1c
theprerakshah <prerakoct2000@gmail.com>
2022-07-19 14:38:38 +0530

    first commit

--- priorityqueue.java
+++ priorityqueue.java
@@ -5,7 +5,8 @@ public class priorityqueue {
 
         PriorityQueue<Integer> prioque = new PriorityQueue<>();
         // if we want queue in decending order then use comparator.reverseorder()
-        // PriorityQueue<Integer> prioque = new PriorityQueue<>(Comparator.reverseOrder());
+        // PriorityQueue<Integer> prioque = new
+        // PriorityQueue<>(Comparator.reverseOrder());
         prioque.offer(8);
         prioque.offer(43);
         prioque.offer(54);
@@ -14,13 +15,14 @@ public class priorityqueue {
         prioque.offer(23);
         prioque.offer(43);
         System.out.println(prioque);
-
+        // to remove the fist element form the queue
         System.out.println(prioque.poll());
         System.out.println(prioque);
-
+        // to print the first element in the queue
         System.out.println(prioque.peek());
         System.out.println(prioque);
 
+        
         prioque.offer(79);
         prioque.offer(100);
         System.out.println(prioque);
